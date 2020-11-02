f2a79dc2b8771371c77d3fa1210f1df2b2d31b99
Dylantjer <73338785+Dylantjer@users.noreply.github.com>
2020-11-02 18:36:27 +0100

    Add files via upload

--- ScriptDylanv1.js
+++ ScriptDylanv1.js
@@ -21,21 +21,31 @@ function invoer(){ // CONTROLE INVOER WOORD
         alert("INCORRECT: Niet 5 letters");
     }
     if( woord == resultaat ){
-        alert("Correct geraden!");
+        alert("Correct geraden, klik op nieuw woord voor een nieuwe woord!");
     }
     else{
-        alert("Incorrect geraden!")
+        alert("Incorrect geraden, probeer opnieuw!")
+        console.log(resultaat[u]);
+        document.getElementById("2A").value = resultaat[0];
+        document.getElementById('invoerveld').value = '';
+     
     }
+
         var vullenvak1 = document.getElementById("invoerveld").value; // EERSTE RIJ ANTWOORD
         document.getElementById("1A").value = woord [0];
         document.getElementById("1B").value = woord [1];
         document.getElementById("1C").value = woord [2];
         document.getElementById("1D").value = woord [3];
         document.getElementById("1E").value = woord [4];
+        
+            
+             
 }
+
  
 function nieuw(){ //RELOAD PAGINA OF TEXTVAK
     var vernieuw = document.getElementById('invoerveld').value = '';
     location.reload();
  }
+ 
  
